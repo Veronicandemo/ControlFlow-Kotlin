f0081292f8136d6208e66de4f55ca779c3723da9
Veronicandemo <ndemoveronica@gmail.com>
2023-03-20 09:14:52 +0300

    more on control flows

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -35,10 +35,10 @@ fun main() {
 //        else -> println("You are soo young")
 //    }
 
-    when(age){
+    when (age) {
         in 21..250 -> println("You can drink in the US")
         in 18..20 -> println("You may vote now")
-        16,17 -> println("You may drive now")
+        16, 17 -> println("You may drive now")
         else -> println("You are too young!")
     }
 
@@ -79,16 +79,16 @@ fun main() {
 //        else -> println("$x is none of the above")
 //    }
 
-    var result = when(x){
+    var result = when (x) {
         is Int -> "is an integer"
         !is Double -> "is not a Double"
         is String -> "is a String"
         else -> "None of the above"
     }
-println("$x $result")
+    println("$x $result")
     var num = 1
 
-    while(num <= 10){
+    while (num <= 10) {
         print("$num ")
         num++
     }
@@ -97,7 +97,7 @@ println("$x $result")
     //Declare a variable
 
     var value = 100;
-    while(value > 0){
+    while (value > 0) {
         println("$value");
         value -= 2;
     }
@@ -106,35 +106,35 @@ println("$x $result")
     do {
         print("$z")
         z++
-    }while (z <= 10)
+    } while (z <= 10)
     println("The do while loop is done")
 
     var feltTemp = "Cold"
     var roomTemp = 10
-    while(feltTemp == "Cold"){
-            roomTemp++
-        if(roomTemp >= 20){
+    while (feltTemp == "Cold") {
+        roomTemp++
+        if (roomTemp >= 20) {
             feltTemp = "Comfy"
             println("Its comfy now")
         }
     }
 
-    for (number in 1..10){
+    for (number in 1..10) {
         print("$number")
     }
-    for(i in 1 until 20){
+    for (i in 1 until 20) {
         print("$i ")
         println("End until")
     }
     //steps of 2 down
-    for(i in 100 downTo 2 step 2){
+    for (i in 100 downTo 2 step 2) {
         print("$i ")
     }
     println("Next loop_______")
     //Write a for loop that runs from 0 to 10000
     //Once it's at 9001 it should write "IT'S OVER 9000!!!"
-    for(w in 0..10000){
-        if(w == 9001){
+    for (w in 0..10000) {
+        if (w == 9001) {
             print("$w ")
             print("ITS OVER 9000!!!")
         }
@@ -145,32 +145,126 @@ println("$x $result")
     //Once the humidityLevel is below 60 it should print "it's comfy now" and set the humidity to "comfy"
     var humidityLevel = 59
     var humidity = "Humid"
-    while (humidityLevel > 60){
+    while (humidityLevel > 60) {
 
         println(humidity)
-       humidityLevel -= 5
-        if(humidityLevel < 60){
+        humidityLevel -= 5
+        if (humidityLevel < 60) {
             println("Its now comfy")
             humidity = "Comfy"
             println(humidity)
         }
     }
 
-    for(i in 1 until 20){
+    for (i in 1 until 20) {
         print("$i ")
-        if(i/2 == 5){
+        if (i / 2 == 5) {
             break
         }
     }
     println("Continue_____")
-    for (i in 1 until 20){
-        if(i/2 == 5){
+    for (i in 1 until 20) {
+        if (i / 2 == 5) {
             // 10/2 == 5
             // 11/ 2 == 5.5 which is 5 in terms of an int
             continue //skip this iteration
         }
         print("$i ")
     }
+    println("End of continue")
+
+    //Kotlin when expression with block of code
+    var day = 2
+    var Dayresult = when (day) {
+        1 -> {
+            "Monday"
+            "First Day of the Week"
+        }
+
+        2 -> {
+            "Tuesday"
+            "Second day of the week"
+        }
+
+        3 -> {
+            "Wednesday"
+            "Third day of the week"
+        }
+
+        4 -> {
+            "Thursday"
+            "Forth day of the week"
+        }
+
+        5 -> {
+            "Friday"
+            "The fifth day of the week"
+        }
+
+        6 -> {
+            "The sixth day of the week"
+            "Saturday"
+        }
+
+        7 -> {
+            "Sunday"
+            "The seventh day of the week"
+        }
+
+        else -> "Invalid day"
+    }
+    println(Dayresult)
+
+    //Kotlin when statement
+    when (day) {
+        1 -> println("Monday")
+        2 -> println("Tuesday")
+        3 -> println("Wednesday")
+        4 -> println("Thursday")
+        5 -> println("Friday")
+        6 -> println("Saturday")
+        7 -> println("Sunday")
+        else -> println("nvalid Day")
+    }
+
+    //combine when conditions
+    when (day) {
+        1, 2, 3, 4, 5 -> println("WeekDay")
+        else -> println("Weekend")
+    }
+    //Range in when condition
+    when (day) {
+        in 1..5 -> println("Weekday")
+        in 5..6 -> println("Weekday") // else
+    }
+
+    //iterate through a range
+    for(item in 1..5){
+        println(item)
+    }
+    //steping down
+    for (item in 15 downTo 1 step  2){
+        println(item)
+    }
+    //iterate through an array
+    var fruits = arrayOf("Mango", "Orange", "Banana", "Apple")
+
+    for(fruit in fruits){
+        println(fruit)
+    }
+
+    //while
+    var i = 5
+    while(i > 0){
+        print("$i ")
+        i--
+    }
+    //do--while loop
+    var w = 10
+    do{
+        print(w)
+        w--
+    }while (w > 0)
 
 }
 
